9499e3ba43f8d77633144259b252e461779969de
Matheus-FSantos <matheus.fs.contato@gmail.com>
2025-06-27 23:15:45 -0300

    feat(task): add base task service project

---
+++ jp-task/src/main/java/io/github/matheus_fsantos/jp_task/JpTaskApplication.java
@@ -0,0 +1,13 @@
+package io.github.matheus_fsantos.jp_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpTaskApplication.class, args);
+	}
+
+}
---
+++ jp-task/src/test/java/io/github/matheus_fsantos/jp_task/JpTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheus_fsantos.jp_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
