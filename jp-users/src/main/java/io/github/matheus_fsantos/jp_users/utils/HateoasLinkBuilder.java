0f5185ddb8942221abfe0fd4a9353c7fd9850827
Matheus-FSantos <matheus.fs.contato@gmail.com>
2025-07-04 10:00:37 -0300

    fix(hateoas): fix hateoas links reference

--- jp-users/src/main/java/io/github/matheus_fsantos/jp_users/utils/HateoasLinkBuilder.java
+++ jp-users/src/main/java/io/github/matheus_fsantos/jp_users/utils/HateoasLinkBuilder.java
@@ -1,9 +1,7 @@
 package io.github.matheus_fsantos.jp_users.utils;
 
-import io.github.matheus_fsantos.jp_users.controller.UserController;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -11,7 +9,7 @@ import java.util.UUID;
 @Component
 public class HateoasLinkBuilder {
     public <T extends RepresentationModel<T>> T addSelfLink(T dto, UUID id) {
-        Link link = WebMvcLinkBuilder.linkTo(UserController.class).slash(id).withSelfRel();
+        Link link = Link.of(String.format("http://localhost:8080/api/users/%s", id)).withSelfRel();
         return dto.add(link);
     }
 }
