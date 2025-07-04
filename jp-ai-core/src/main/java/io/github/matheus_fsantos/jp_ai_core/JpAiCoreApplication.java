470fc332010ac85e6bddcb8ad2ccb9dcb1863e2d
Matheus-FSantos <matheus.fs.contato@gmail.com>
2025-07-04 12:27:04 -0300

    feat(ai-code): add ai core service

---
+++ jp-ai-core/src/main/java/io/github/matheus_fsantos/jp_ai_core/JpAiCoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.matheus_fsantos.jp_ai_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAiCoreApplication.class, args);
+	}
+
+}
---
+++ jp-ai-core/src/test/java/io/github/matheus_fsantos/jp_ai_core/JpAiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheus_fsantos.jp_ai_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAiCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
